d19bfc7e909430043c0c1cd5bcc0d456da54d678
Daniel Wiberg <wibbe@blueeye.se>
2013-05-13 22:02:46 +0200

    Added a statistics monitor

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,8 @@
 
 require([
   "util/domReady!",
-  "util/gl-util"
+  "util/gl-util",
+  "util/stats"
 ], function(doc, glUtil) {
   "use strict";
   var canvas = document.getElementById("game-canvas");
@@ -13,10 +14,22 @@ require([
     return;
   }
 
+  // Create and add the stats object
+  var stats = new Stats();
+  stats.setMode(1);
+
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+
+  document.body.appendChild(stats.domElement);
+
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 
   glUtil.start(function() {
+    stats.begin();
     // Drawing code goes here!
+    stats.end();
   });
 });
